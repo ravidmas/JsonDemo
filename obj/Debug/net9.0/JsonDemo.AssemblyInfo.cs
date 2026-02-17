21237f97e2daf7ee0d954d423c8abdeaa709ea89
yuvalvolinski <ravidmastey@gmail.com>
2026-02-17 11:56:58 +0200

    init

--- obj/Debug/net9.0/JsonDemo.AssemblyInfo.cs
+++ obj/Debug/net9.0/JsonDemo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JsonDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+800ec44076109c74e41f5fdde3815df7536f7e77")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27034d5cbc7b9b45658ca06928142c94309c55cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("JsonDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JsonDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
