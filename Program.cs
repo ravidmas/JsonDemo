27034d5cbc7b9b45658ca06928142c94309c55cb
yuvalvolinski <ravidmastey@gmail.com>
2026-02-17 11:56:19 +0200

    what

--- Program.cs
+++ Program.cs
@@ -71,7 +71,6 @@ List<Chord> chords = new()
 app.MapGet("/getChords", (string? difficulty, int? count) =>
 {
     if (difficulty == "all") return Results.Ok(chords);
-
     List<Chord> chordList = new List<Chord>();
 
     if (difficulty == "easy")
